4c183151ad409c8ec28892726ddb35151358e5cc
Ravi <i.rrpatel@gmail.com>
2017-04-20 14:08:06 -0500

    add decrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -15,4 +15,27 @@ def encrypt(str)
 	str
 end
 
-p encrypt("pass word")
+
+# Method called decrypt that takes a string as an argument
+# Loop through each character of the string
+# Check each character to see if it's a space
+# If not a space, then...
+# Modify the character to be the letter right before it
+
+def decrypt(str)
+	index = 0
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+	while index < str.length
+		if str[index] != " "
+			letter_index = alphabet.index(str[index])
+			str[index] = alphabet[letter_index - 1]
+		end
+		index += 1
+	end
+	str
+end
+
+p "Encrypted password:"
+p encrypt('pass word')
+p "Decrypted password:"
+p decrypt(encrypt('pass word'))
