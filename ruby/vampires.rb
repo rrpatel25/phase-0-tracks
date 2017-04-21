fee62796f29c2eb3c807a12d72259e55f3acd156
Ravi <i.rrpatel@gmail.com>
2017-04-21 14:58:59 -0500

    Add interview questions to detect vampire

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+# Interview with the Vampire
+
+puts "What is your name?"
+werewolf_name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "Oh really? So in what year were you born?"
+birth_year = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread_question = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp
