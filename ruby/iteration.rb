7ac56903953c6d6e7a378775518d9d85946e25be
Ravi <i.rrpatel@gmail.com>
2017-04-26 18:31:25 -0500

    add code to show functionality of .each and .map

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,3 +6,36 @@ def status_message
 end
 
 status_message { |activity| puts "Let's go #{activity}!" }
+
+# Release 1
+fifa = ["EPL","LaLiga","MLS","Bundesliga"]
+
+clubs = { 
+epl: "Man u",  
+Laliga: "Barcelona",
+MLS: "Chicago Fire",
+Bundesliga: "Brayern Munich"
+}
+
+# initial .each
+new_soccer = fifa.length
+
+fifa.each do |soccer| 
+  puts soccer
+end 
+
+# post .each 
+fifa.each do |soccer|
+puts soccer
+puts new_soccer
+end
+# initial .each for teams
+
+clubs.each do |teams,namu |
+  puts "the name of the best team in #{teams} is #{namu}"
+end
+
+# initial .map! for array
+fifa.map! do |soccer|
+  puts soccer.reverse
+end
