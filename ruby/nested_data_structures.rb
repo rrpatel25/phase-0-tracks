7afe9db4d6da0efbdbe61042e8e12d46794e06ff
Ravi <i.rrpatel@gmail.com>
2017-04-27 19:14:22 -0500

    build a nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,58 @@
+nba = {
+	chicago: {
+		team_name: 'Bulls',
+		best_player: 'Jimmy Butler',
+		key_features: [
+			'Benny the Bull',
+			'The Luvabulls',
+			'Michael Jordan',
+			numbers: {
+				championships: 6,
+				player_number: 23
+			}
+		]
+	},
+	los_angelas: {
+		team_name: [
+			'Lakers',
+			'Clippers'
+		],
+		best_player: 'Chris Paul',
+		key_features: [
+			Celebs: {
+				'Jack Nicholson',
+				'Matt Damon',
+				'Kobe Bryant',
+				'Steve Balmer'
+			},
+			'Staples Center',
+			numbers: {
+				championships: 16,
+				player_number: [
+					24,
+					32,
+					44
+				]
+			}
+		]
+	},
+	new_york: {
+		team_name: [
+			'Knicks',
+			'Nets'
+		]
+		best_player: 'Carmelo Anthony',
+		key_features: [
+			'Spike Lee',
+			'Madison Square Garden',
+			'Jay-Z',
+			numbers: {
+				championships: 4,
+				player_number: [
+					5,
+					33
+				]
+			}
+		]
+	}
+}
