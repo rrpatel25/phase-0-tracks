8153f6016b592065ef15ba008fde73204f809b0e
Ravi <i.rrpatel@gmail.com>
2017-04-27 20:20:11 -0500

    add prints of deeply nested data

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,3 +1,5 @@
+# Nested Data Structure Featuring NBA Cities+Teams
+
 nba = {
 	chicago: {
 		team_name: 'Bulls',
@@ -18,14 +20,14 @@ nba = {
 			'Clippers'
 		],
 		best_player: 'Chris Paul',
-		key_features: [
-			Celebs: {
+		key_features: {
+			celebs: [
 				'Jack Nicholson',
 				'Matt Damon',
 				'Kobe Bryant',
 				'Steve Balmer'
-			},
-			'Staples Center',
+			],
+			place: 'Staples Center',
 			numbers: {
 				championships: 16,
 				player_number: [
@@ -34,13 +36,13 @@ nba = {
 					44
 				]
 			}
-		]
+		}
 	},
 	new_york: {
 		team_name: [
 			'Knicks',
 			'Nets'
-		]
+		],
 		best_player: 'Carmelo Anthony',
 		key_features: [
 			'Spike Lee',
@@ -56,3 +58,17 @@ nba = {
 		]
 	}
 }
+
+# Calling a few pieces from the nested data
+
+puts 'Here is a key icon/figure who is associated with Chicago:'
+p nba[:chicago][:key_features][2]
+
+puts 'Here is where the famous Lakers play:'
+p nba[:los_angelas][:key_features][:place]
+
+puts 'And the player-number of the new Lakers Head of Operations Magic Johnson:'
+p nba[:los_angelas][:key_features][:numbers][:player_number][1]
+
+puts 'Here is the reversed name of Nets part-owner Jay-Z:'
+p nba[:new_york][:key_features][2].reverse
