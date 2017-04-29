ba0c76617d9f057cd4f98997f7cc852a1890473f
Ravi <i.rrpatel@gmail.com>
2017-04-29 16:52:48 -0500

    add method for Fibonacci numbers

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,4 +1,5 @@
 # Release 0: Solving Problems with Data Structures
+
 arr = [42, 89, 23, 1]
 
 def search_array (array, integer)
@@ -13,3 +14,31 @@ def search_array (array, integer)
     end
   end
 end
+
+
+# Release 1: Calculate Fibonacci Numbers
+
+def fib(length)
+# The original array
+  array = [0, 1]
+# i-counter will always be one ahead of the length(input) parameter
+  i = 1 
+# Taking care of the edge cases
+  if length == 0
+    array = []
+  elsif length == 1
+    array = [0]
+  elsif length == 2
+    array
+  else 
+# Take array[i] + array[i-1] then add to the original array
+    while i < length - 1
+      array.push(array[i] + array[i-1])
+      i += 1
+    end
+    array
+  end
+end
+
+p fib(100)[99] == 218922995834555169026 
+# return => true
