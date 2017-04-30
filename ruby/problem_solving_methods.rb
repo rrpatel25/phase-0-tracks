121b093095b0d9a31f02c02138a9bfff0706f2b4
Ravi <i.rrpatel@gmail.com>
2017-04-29 19:09:10 -0500

    add bubble sort method for sorting an array

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -40,5 +40,59 @@ def fib(length)
   end
 end
 
+#Print result for lenth of 100 (which is @ index position 99)
 p fib(100)[99] == 218922995834555169026 
 # return => true
+
+
+
+# Release 2: Sort an Array
+
+array = [9, 7, 1, 8, 3, 9, 1, 7, 5, 4, 9, 2, 1]
+
+# take in an array as an argument
+def bubble_sort(array)
+
+  # set variables
+  i = 0
+  swapped = false
+
+# initiate a loop
+  while i < array.length - 1
+    # set up temporary variables to hold array items
+    a = array[i]
+    b = array[i+1]
+    # if a and b are in the correct order...
+    if a < b || a == b
+      i += 1
+    else # if a and b are in an incorrect order...
+      array[i] = b
+      array[i+1] = a
+      swapped = true
+      i += 1
+    end
+    # Is sorting complete (swapped remains false)? If any were in an incorrect order, rerun bubble_sort on array
+    if swapped
+      bubble_sort(array)
+    end
+  end
+  # return array
+  array
+end
+
+p bubble_sort(array)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
