e0a5dc52e663c306614b875dd2ed0b552787f1cf
Ravi <i.rrpatel@gmail.com>
2017-05-01 09:19:02 -0500

    add temporary answers for rest of problems

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -33,9 +33,9 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # printing each item in the array separated by an asterisk
 # ----
 
-# zombie_apocalypse_supplies.each do |item|
-# 	p item, "*"
-# end
+zombie_apocalypse_supplies.each do |item|
+	p item, "*"
+end
 
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
 # in alphabetical order. Do not use any special built-in methods.
@@ -64,37 +64,37 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # end
 
 
-###### def alphabetizer(arr)
-# 	i = 0
-# 	while i < arr.length-1
-# 		element = arr[i]
-# 		compare = arr[i+1]
-# 		if element[0] <= compare[0]
-# 			j = 1
-# 			while element[0] == compare[0] && element.length >= j && compare.length >= j
-# 				if(element[j] < compare[j])
-# 					break
-# 				elsif (element[j] > compare[j])
-# 					arr[i] = compare
-# 					arr[i+1] = element
-# 					break
-# 				else
-# 					j = j+1
-# 				end
-# 			end
-# 			i=i+1
-# 		else
-# 			arr[i] = compare
-# 			arr[i+1] = element
-# 			p arr
-# 			i = 0
-# 		end
-# 		#alphabetizer(arr)
-# 	end
-# 	arr
-# end
+##### def alphabetizer(arr)
+	i = 0
+	while i < arr.length-1
+		element = arr[i]
+		compare = arr[i+1]
+		if element[0] <= compare[0]
+			j = 1
+			while element[0] == compare[0] && element.length >= j && compare.length >= j
+				if(element[j] < compare[j])
+					break
+				elsif (element[j] > compare[j])
+					arr[i] = compare
+					arr[i+1] = element
+					break
+				else
+					j = j+1
+				end
+			end
+			i=i+1
+		else
+			arr[i] = compare
+			arr[i+1] = element
+			p arr
+			i = 0
+		end
+		#alphabetizer(arr)
+	end
+	arr
+end
 
-# p alphabetizer(zombie_apocalypse_supplies)
+p alphabetizer(zombie_apocalypse_supplies)
 
 # def alphabetizer(arr)
 # 	index = 0
@@ -121,24 +121,24 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # ----
 
 
-# def item_searcher(string)
-# 	zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
-#                               "shotgun", "compass", "CB radio", "batteries"]
-# 	zombie_apocalypse_supplies.each do |item|
-# 		if item == string
-# 			output = true
-# 		else
-# 			output = false
-# 		end
-# 		if output == true
-# 			puts "Yes!"
-# 		else
-# 			nil
-# 		end
-# 	end
-# end
+def item_searcher(string)
+	zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
+                              "shotgun", "compass", "CB radio", "batteries"]
+	zombie_apocalypse_supplies.each do |item|
+		if item == string
+			output = true
+		else
+			output = false
+		end
+		if output == true
+			puts "Yes!"
+		else
+			nil
+		end
+	end
+end
 
-# p item_searcher("rations")
+p item_searcher("rations")
 
 
 # 4. You can't carry too many things, you've only got room in your pack for 5.
@@ -152,6 +152,31 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 
 # end
 
+# new_bag = []
+
+# zombie_apocalypse_supplies.each do |supply|
+# 	counter = 4
+# 	i = 0
+
+# 	while counter >= 0
+# 		new_bag.push(zombie_apocalypse_supplies[i])
+# 		i += 1
+# 		counter -= 1
+# 	end
+# end
+
+# p new_bag
+
+new_bag = []
+counter = 0
+until counter == 5
+	new_bag.push(zombie_apocalypse_supplies[counter])
+	counter += 1
+end
+
+p new_bag
+
+
 # 5. You found another survivor! This means you can combine your supplies.
 # Create a new combined supplies list out of your zombie_apocalypse_supplies
 # and their supplies below. You should get rid of any duplicate items.
@@ -161,13 +186,13 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 #                             "solar battery", "flashlight"]
 # # ----
 
-# zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
-#                               "shotgun", "compass", "CB radio", "batteries"]
+zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
+                              "shotgun", "compass", "CB radio", "batteries"]
 
-# new_list = zombie_apocalypse_supplies + other_survivor_supplies
-# new_list.uniq
+new_list = zombie_apocalypse_supplies + other_survivor_supplies
+new_list.uniq
 
-# p new_list.uniq
+p new_list.uniq
 
 # Hash Drills
 
@@ -185,9 +210,9 @@ extinct_animals = {
 # with a dash in between the key and value, and an asterisk between each pair.
 # ----
 
-# extinct_animals.each do |animal,date|
-# 	p "*#{animal}-#{date}*"
-# end
+extinct_animals.each do |animal,date|
+	p "*#{animal}-#{date}*"
+end
 
 
 
@@ -207,22 +232,38 @@ extinct_animals = {
 
 # p extincter(extinct_animals)
 
-def extinct(list)
-	list.each do |animal,date|
-		if [:date] > 2000
-			[:animal] = nil
-		end
+# def extinct(list)
+# 	list.each do |animal,date|
+# 		if [:date] > 2000
+# 			[:animal] = nil
+# 		end
+# 	end
+# 	list
+# end
+
+new_list = {}
+extinct_animals.each do |animal,date|
+	if date.to_i < 2000
+		new_list[animal] = date
 	end
-	list
 end
 
-p extinct(extinct_animals)
+p new_list
+
+
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # so they accurately reflect what year the animal went extinct.
 # Do not use any special built-in methods.
 # ----
+
+extinct_animals.each do |animal,date|
+	new_date = date - 3
+	extinct_animals[animal] = new_date
+end
+
+p extinct_animals
 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
 # Check if they're included in extinct_animals, one by one:
@@ -232,8 +273,29 @@ p extinct(extinct_animals)
 # Do not use any special built-in methods.
 # ----
 
+extinct_animals.each do |animal,date|
+	if animal == "Andean Cat"
+		puts 'Extinct'
+	elsif animal == "Dodo"
+		puts 'Extinct'
+	elsif animal == "Saiga Antelope"
+		puts 'Extinct'
+	else
+		puts 'Still around!'
+	end
+end
+
+p extinct_animals
+		
+
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
 # Remove them from extinct_animals and return the key value pair as a two item array.
 # Find the built-in method that helps you accomplish this in the Ruby documentation
 # for Hashes.
 # ----
+
+extinct_animals.delete_if do |animal,date|
+	animal == "Passenger Pigeon"
+end
+
+p extinct_animals
