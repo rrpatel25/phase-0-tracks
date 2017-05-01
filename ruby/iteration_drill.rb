d8ea618b3ea6da14e2036bc1063454becac516df
Ravi <i.rrpatel@gmail.com>
2017-05-01 00:51:06 -0500

    add work for more problems/still work in-progress

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -64,37 +64,37 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # end
 
 
-def alphabetizer(arr)
-	i = 0
-	while i < arr.length-1
-		element = arr[i]
-		compare = arr[i+1]
-		if element[0] <= compare[0]
-			j = 1
-			while element[0] == compare[0] && element.length >= j && compare.length >= j
-				if(element[j] < compare[j])
-					break
-				elsif (element[j] > compare[j])
-					arr[i] = compare
-					arr[i+1] = element
-					break
-				else
-					j = j+1
-				end
-			end
-			i=i+1
-		else
-			arr[i] = compare
-			arr[i+1] = element
-			p arr
-			i = 0
-		end
-		#alphabetizer(arr)
-	end
-	arr
-end
+###### def alphabetizer(arr)
+# 	i = 0
+# 	while i < arr.length-1
+# 		element = arr[i]
+# 		compare = arr[i+1]
+# 		if element[0] <= compare[0]
+# 			j = 1
+# 			while element[0] == compare[0] && element.length >= j && compare.length >= j
+# 				if(element[j] < compare[j])
+# 					break
+# 				elsif (element[j] > compare[j])
+# 					arr[i] = compare
+# 					arr[i+1] = element
+# 					break
+# 				else
+# 					j = j+1
+# 				end
+# 			end
+# 			i=i+1
+# 		else
+# 			arr[i] = compare
+# 			arr[i+1] = element
+# 			p arr
+# 			i = 0
+# 		end
+# 		#alphabetizer(arr)
+# 	end
+# 	arr
+# end
 
-p alphabetizer(zombie_apocalypse_supplies)
+# p alphabetizer(zombie_apocalypse_supplies)
 
 # def alphabetizer(arr)
 # 	index = 0
@@ -120,13 +120,35 @@ p alphabetizer(zombie_apocalypse_supplies)
 # For instance: are boots in your list of supplies?
 # ----
 
+
+# def item_searcher(string)
+# 	zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
+#                               "shotgun", "compass", "CB radio", "batteries"]
+# 	zombie_apocalypse_supplies.each do |item|
+# 		if item == string
+# 			output = true
+# 		else
+# 			output = false
+# 		end
+# 		if output == true
+# 			puts "Yes!"
+# 		else
+# 			nil
+# 		end
+# 	end
+# end
+
+# p item_searcher("rations")
+
+
 # 4. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
 # leaving only 5. Do not use any special built-in methods.
 # ----
 
 # def item_remover(arr)
-
+# 	while arr.length > 5
+# 		arr[arr]
 
 # end
 
@@ -135,9 +157,17 @@ p alphabetizer(zombie_apocalypse_supplies)
 # and their supplies below. You should get rid of any duplicate items.
 # Find the built-in method that helps you accomplish this in the Ruby
 # documentation for Arrays.
-other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
-                            "solar battery", "flashlight"]
-# ----
+# other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
+#                             "solar battery", "flashlight"]
+# # ----
+
+# zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
+#                               "shotgun", "compass", "CB radio", "batteries"]
+
+# new_list = zombie_apocalypse_supplies + other_survivor_supplies
+# new_list.uniq
+
+# p new_list.uniq
 
 # Hash Drills
 
@@ -155,9 +185,38 @@ extinct_animals = {
 # with a dash in between the key and value, and an asterisk between each pair.
 # ----
 
+# extinct_animals.each do |animal,date|
+# 	p "*#{animal}-#{date}*"
+# end
+
+
+
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000. Do not use any special built-in methods.
 # ----
+
+# def extincter(arr)
+# 	special_list = []
+# 	arr.each do |animal,date|
+# 		if date < 2000 { |animal,date|
+# 			special_list << animal date }
+# 		end
+# 	end
+# 	special_list
+# end
+
+# p extincter(extinct_animals)
+
+def extinct(list)
+	list.each do |animal,date|
+		if [:date] > 2000
+			[:animal] = nil
+		end
+	end
+	list
+end
+
+p extinct(extinct_animals)
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
