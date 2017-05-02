65541e342cb694ca07db4195350d1e09c9a755fb
Ravi <i.rrpatel@gmail.com>
2017-05-02 07:44:41 -0500

    add puppy class, attributes, and methods

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# Puppy CLASS (species)
+# Furrycious Cutatous
+
+# Puppy ATTRIBUTES (characteristics)
+# Leg Count: 4
+# Friendly: yes
+# Name: varies
+# Color: varies
+# Size: varies
+
+# Puppy METHODS (behavior)
+# Sniff
+# Walk around
+# Lick
+# Sleep
