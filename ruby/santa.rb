72369caa7086243b2ab0681f8d05a9cef4843be5
Ravi <i.rrpatel@gmail.com>
2017-05-03 08:52:35 -0500

    add attributes to santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,11 @@
 class Santa
-	def initialize
+	def initialize(gender, ethnicity)
 		puts 'Initializing Santa instance...'
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", 
+			"Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 
 	def speak
@@ -10,8 +15,16 @@ class Santa
 	def eat_milk_and_cookies(cookie_type)
 		puts "That was a good #{cookie_type} cookie!"
 	end
+
+	def about
+		puts "Gender: #{@gender}"
+		puts "Ethnicity: #{@ethnicity}"
+		puts "Reindeer_ranking: #{@reindeer_ranking}"
+		puts "Age: #{@age}"
+	end
 end
 
-santa = Santa.new
+santa = Santa.new("Male", "Canadian")
 p santa.speak
 p santa.eat_milk_and_cookies("chocolate chip")
+p santa.about
