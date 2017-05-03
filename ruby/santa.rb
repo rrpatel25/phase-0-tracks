67a1138b553fad4f3e074ba5b4db62fa51ec4e97
Ravi <i.rrpatel@gmail.com>
2017-05-03 10:52:43 -0500

    add diverse initializations to make diverse instances

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -28,3 +28,13 @@ santa = Santa.new("Male", "Canadian")
 p santa.speak
 p santa.eat_milk_and_cookies("chocolate chip")
 p santa.about
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", 
+	"prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+p santas
