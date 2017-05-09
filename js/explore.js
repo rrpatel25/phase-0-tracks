78579d48e177381e4816add42a0a75e9e87eea54
Ravi <i.rrpatel@gmail.com>
2017-05-09 18:30:40 -0500

    Add pseudocode for reverse function

--- js/explore.js
+++ js/explore.js
@@ -1,4 +1,11 @@
 // Pseudocoding a reverse function
 
 // For the reverse function, there is a string input
-// 
+// The function will take the length of the string
+// and while some counter is less than the length of the string
+// the body of the function will take the last letter of the string input
+// and add it to a new empty string.
+// The counter will move up 1, causing the function to check the second-to-last letter
+// and adding it to the empty string containing the 1 previous letter.
+// This function goes on for as long as the length of the string.
+
