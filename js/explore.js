c0e648aff1008041b5ea3efaeb4c23ef9a20cd30
Ravi <i.rrpatel@gmail.com>
2017-05-09 21:40:27 -0500

    Add code to create functioning reverse function

--- js/explore.js
+++ js/explore.js
@@ -1,11 +1,25 @@
 // Pseudocoding a reverse function
 
 // For the reverse function, there is a string input
-// The function will take the length of the string
-// and while some counter is less than the length of the string
-// the body of the function will take the last letter of the string input
-// and add it to a new empty string.
-// The counter will move up 1, causing the function to check the second-to-last letter
-// and adding it to the empty string containing the 1 previous letter.
-// This function goes on for as long as the length of the string.
+// The function will take the length of the string and subtract one
+// 	and while that number is greater than 0,
+// 	the body of the function will take the last letter of the string input
+// 	and add it to a new empty string.
+// The counter/number will subtract 1, causing the function to check the second-to-last letter
+// 	and adding it to the empty string containing the letter at the new index.
+// This function goes on for as long as the number is greater than 0.
 
+
+
+// JS Function
+function reverse(string) {
+	var output = "";
+	for (var strIndex = string.length - 1; strIndex >= 0; strIndex--) {
+		output += string[strIndex];
+	};
+	return (output);
+}
+
+
+// Driver Code
+console.log(reverse("hello"));
