82875e94058d87242d09857f7d9838d4cec88672
Ravi <i.rrpatel@gmail.com>
2017-05-14 17:41:01 -0500

    Add driver code to test function

--- js/algos.js
+++ js/algos.js
@@ -4,11 +4,30 @@
 // Release 0: Find the longest phrase
 
 // Given an array of elements containing different words/phrases
-// Starting from the beginning (index of 0), compare the length of the item with 
-// the length of the item next to it.
-// If the first item is longer, set it equal to longest variable
-// Move to the next comparison of items and continue until the end of the array
-// is reached. 
-// At the end, print the longest variable
+// Set the first element equal to longest. 
+// Starting with a counter 1, while it's less than the length of the array,
+// If the element at the position of the counter is greater than the current
+// longest element, set the longest element equal to the new element.
+// This continues as the counter is incremented by 1 every cycle, while remaining
+// less than the length of the array.
+// At the end, return the longest variable.
 
-var array = ["long phrase","longest phrase","longer phrase"];
+var array = ["long phrase","longest phrase","longer phrase","longlonglong phrase"];
+
+function longestElement(array) {
+	if (array.length > 0) {
+		var longest = array[0];
+		for (var i = 1; i < array.length; i++) {
+			if (longest < array[i]) {
+				longest = array[i];
+			}
+		}
+		return longest;
+	}
+}
+
+// Driver Code
+console.log(longestElement(array));
+
+
+
