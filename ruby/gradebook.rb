fa499c5326b5daae2cc1a25c6fdaba6cbfabc35f
Ravi <i.rrpatel@gmail.com>
2017-05-21 18:17:54 -0400

    Initial commit

---
+++ ruby/gradebook.rb
@@ -0,0 +1,22 @@
+# The Book of Grades (TM)
+
+# MAIN OBJECTIVE: Entering students grade into the gradebook
+# and Retrieve grades when needed
+
+# Provide an input of studnet name, output of all of their grades
+
+# All of the grades for a given assignment/test (besides student's name)
+
+class Grades
+	def initialize
+		@student = student_name
+		@assignment = assignment_name
+		@grade = grade_value
+	end
+
+
+
+
+
+
+end
