0605ca97e98c7d21c4bd283dbb99b3fa3312b211
Ravi <i.rrpatel@gmail.com>
2017-05-21 18:32:46 -0400

    Add grades, students, and assignments classes

--- ruby/gradebook.rb
+++ ruby/gradebook.rb
@@ -9,14 +9,25 @@
 
 class Grades
 	def initialize
-		@student = student_name
-		@assignment = assignment_name
-		@grade = grade_value
+		@student_id = student_id
+		@assignment_id = assignment_id
+		@score = assignment_score
+		@completed_date = completed_date
 	end
 
+end
 
+class Students
+	def initialize
+		@first_name = first_name
+		@last_name = last_name
+	end
 
+end
 
-
-
+class Assignments
+	def initialize
+		@assignment_name = assignment_name
+		@due_date = due_date
+	end
 end
