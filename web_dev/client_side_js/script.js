b0a3cbb1b761e439ee6f22aac356aaa41855c561
Ravi <i.rrpatel@gmail.com>
2017-05-25 17:41:05 -0500

    Add click feature for title message

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -3,3 +3,7 @@ console.log("Hello world");
 contactList.addEventListener("mouseover", function ( event ) {
 	event.target.style.color = "orange";
 }, false);
+
+welcomeHead.addEventListener("click", function( event ) {
+  event.target.style.color = "green";
+}, false);
